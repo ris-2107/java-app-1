9156e522b19abd8b5712c463cd13156ad7e8d91e
ris2107 <rishabh210798@gmail.com>
2024-04-05 23:15:48 +0530

    Initial Commit

---
+++ src/test/java/com/example/javaapp1/JavaApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
